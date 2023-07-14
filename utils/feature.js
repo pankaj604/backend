5618b3b3d887db6b2c67ade40fa710dc30ac0fbc
DELL <pkewat604@gmail.com>
2023-07-14 23:47:01 +0530

    auto logout error test1

--- utils/feature.js
+++ utils/feature.js
@@ -7,8 +7,8 @@ export const sendCookie = (user, res, message, statusCode = 200) => {
     .cookie("token", token, {
       httpOnly: true,
       maxAge: 15*  6000 * 100000,
-      sameSite: process.env.NODE_ENV === "Development" ? "lax" : "none",
-      secure: process.env.NODE_ENV === "Development" ? false : true,
+      // sameSite: process.env.NODE_ENV === "Development" ? "lax" : "none",
+      // secure: process.env.NODE_ENV === "Development" ? false : true,
     })
     .json({
       success: true,
