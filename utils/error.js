b5e8b9805e2e959ce1aba833ebc3f38370e1317b
pankaj604 <pkewat604@gmail.com>
2023-09-08 15:21:45 +0530

    error name chaneg

--- utils/error.js
+++ utils/error.js
@@ -1,17 +1,17 @@
 class ErrorHandler extends Error {
-  constructor(message, statusCode) {
-    super(message);
+  constructor(massage, statusCode) {
+    super(massage);
     this.statusCode = statusCode;
   }
 }
 
 export const errorMiddleware = (err, req, res, next) => {
-  err.message = err.message || "Internal Server Error";
+  err.massage = err.massage || "Internal Server Error";
   err.statusCode = err.statusCode || 500; 
 
   return res.status(err.statusCode).json({
     success: false,
-    message: err.message,
+    massage: err.massage,
   });
 };
 
