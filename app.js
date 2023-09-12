3a816e36f7fe63d081e650c31a4998dad3a8901b
pankaj604 <pkewat604@gmail.com>
2023-09-12 12:40:30 +0530

    sitemap chack

--- app.js
+++ app.js
@@ -41,9 +41,6 @@ app.use("/v1/hostel", hostelrouter);
 app.get("/", (req, res) => {
   res.sendFile(path.resolve("build", "index.html"));
 });
-app.get("/sitemap", (req, res) => {
-  res.sendFile(path.resolve("build", "sitemap.xml"));
-});
 app.get("/*", (req, res) => {
   res.sendFile(path.resolve("build", "index.html"));
 });
