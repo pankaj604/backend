d027fb4f8724e3a97f7c1cc98674acd4a8e8a7cf
pankaj604 <pkewat604@gmail.com>
2023-09-13 21:17:06 +0530

    chack

--- app.js
+++ app.js
@@ -69,27 +69,7 @@ app.use("/v1/hostel", hostelrouter);
 app.get("/", (req, res) => {
   res.sendFile(path.resolve("build", "index.html"));
 });
-//
 
-app.get("/sitemap", (req, res) => {
-  const xmldata = (
-    <urlset
-      xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-      xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
-              http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"
-    >
-      {/* <!-- created with Free Online Sitemap Generator www.xml-sitemaps.com --> */}
-
-      <url>
-        <loc>https://www.wellroom.in/</loc>
-        <lastmod>2023-09-12T07:32:34+00:00</lastmod>
-      </url>
-    </urlset>
-  );
-
-  res.send(xmldata);
-});
 
 //np
 app.get("/*", (req, res) => {
