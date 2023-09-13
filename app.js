7bb3d4e8f7831625d08fb23a184fc7f9c7a37eb7
pankaj604 <pkewat604@gmail.com>
2023-09-13 21:14:19 +0530

    sitemap

--- app.js
+++ app.js
@@ -30,9 +30,11 @@ app.use(
 );
 
 const blockLocalhost = (req, res, next) => {
-  if (req.headers.host && req.headers.host.startsWith('localhost')) {
+  if (req.headers.host && req.headers.host.startsWith("localhost")) {
     // Reject the request with a forbidden (403) status code
-    return res.status(403).json({ message: 'Access from localhost is not allowed.' });
+    return res
+      .status(403)
+      .json({ message: "Access from localhost is not allowed." });
   }
   next();
 };
@@ -41,14 +43,14 @@ const blockLocalhost = (req, res, next) => {
 app.use(blockLocalhost);
 //
 
-const allowedOrigins = ['https://wellroom.in'];
+const allowedOrigins = ["https://wellroom.in"];
 
 const corsOptions = {
   origin: function (origin, callback) {
     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
-      callback(new Error('Not allowed by CORS'));
+      callback(new Error("Not allowed by CORS"));
     }
   },
 };
@@ -67,9 +69,29 @@ app.use("/v1/hostel", hostelrouter);
 app.get("/", (req, res) => {
   res.sendFile(path.resolve("build", "index.html"));
 });
-app.get("/sitemap.xml", (req, res) => {
-  res.sendFile(path.resolve("build", "sitemap.xml"));
+//
+
+app.get("/sitemap", (req, res) => {
+  const xmldata = (
+    <urlset
+      xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+      xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+              http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"
+    >
+      {/* <!-- created with Free Online Sitemap Generator www.xml-sitemaps.com --> */}
+
+      <url>
+        <loc>https://www.wellroom.in/</loc>
+        <lastmod>2023-09-12T07:32:34+00:00</lastmod>
+      </url>
+    </urlset>
+  );
+
+  res.send(xmldata);
 });
+
+//np
 app.get("/*", (req, res) => {
   res.sendFile(path.resolve("build", "index.html"));
 });
